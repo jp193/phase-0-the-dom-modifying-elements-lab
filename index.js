b45aab942b220289353d9d599e18d7a53111a321
Jamal Portericker <jportericker@yahoo.com>
2023-04-28 13:40:45 -0400

    dom

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove()
+
+// const newHeader = document.getElementsByTagName("body");
+// body.innerHTML =  "<h1>Poodles!</h1>"
+
+
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Jamal Portericker is the champion"
